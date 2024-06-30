931aba777f898380ab23fe923268cae6629f8f3d
Aakriti mehrotra <aakriti.meh.2004@gmail.com>
2024-06-30 22:45:50 +0530

    Mobile-size styles updated

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -84,8 +84,14 @@ const LandingPage = () => {
                     <br />
                 </div>
                 <div className="LCdescription">
-                Sign-in to interact with a community of fellow hobbyists and an eco-system of experts, teachers, suppliers, classes, workshops, and places to practice, participate or perform. Your hobby may be about visual or performing arts, sports, games, gardening, model making, cooking, indoor or outdoor activities…<br />
-                If you are an expert or a seller, you can Add your Listing and promote yourself, your students, products,  services or events.  Hop on your hobbyhorse and enjoy the ride.<br />
+                <p className="LC-desktop-text">
+                    Sign-in to interact with a community of fellow hobbyists and an eco-system of experts, teachers, suppliers, classes, workshops, and places to practice, participate or perform. Your hobby may be about visual or performing arts, sports, games, gardening, model making, cooking, indoor or outdoor activities…<br />
+                    If you are an expert or a seller, you can Add your Listing and promote yourself, your students, products,  services or events.  Hop on your hobbyhorse and enjoy the ride.
+                </p>
+                <p className="LC-mobile-text">
+                Sign-in to interact with a community of fellow hobbyists and an eco-system of experts, teachers, suppliers, classes, workshops, and places to practice, participate or perform.
+                </p>
+
                 <br />
                 </div>
                 <div className="LCimage">
